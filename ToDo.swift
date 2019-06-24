4b9750762d57958db6a8fc201e8dcc6d8f9b6dd9
sara shelton <saranic314@gmail.com>
2019-06-24 11:16:01 -0500

    iteration 0

---
+++ ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by sara shelton on 6/24/19.
+//  Copyright © 2019 sara shelton. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+
+    var name = ""
+    var important = false
+    
+}
---
+++ toDoList/ToDoTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ToDoTableViewController.swift
+//  toDoList
+//
+//  Created by sara shelton on 6/24/19.
+//  Copyright © 2019 sara shelton. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos : [ToDo] = []
+    
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        // important is set to false by default
+        
+        return [swift, dog]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toDos = createToDos()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        
+        return cell
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
